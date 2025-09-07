692cd96647841fc5b713e9d9d66e6f7beecd0e2e
Hai <danghoanghai042@gmail.com>
2025-09-07 22:22:18 +0700

    add requirements and readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Web dự đoán rò rỉ nước
+#Web dự đoán rò rỉ nước
+
+###Yêu cầu:
+- Cài đặt các thư viện cần thiết
+- MySQL 
+
+```sh
+pip install -r requirement.txt
+```
+
+###Chạy:
+```sh
+uvicorn app:app --reload 
+```
+
+- App sẽ chạy ở `http://localhost:8000`
--- add_data.py
+++ add_data.py
@@ -1,24 +1,17 @@
-# import pandas as pd
-# from sqlalchemy import create_engine
+import pandas as pd
+from sqlalchemy import create_engine
 
-# # Đọc CSV
-# df = pd.read_csv("D:/Huce/Coll_2/tttn/data/raw_data/Van_Dau8/luu_luong.csv")   # nếu file dạng tab-separated
-# # Hoặc df = pd.read_excel("data.xlsx")
+df = pd.read_csv("/luu_luong.csv")
 
-# # Chuẩn hóa tên cột
-# df.columns = ["stt", "time", "datetime", "pressure", "total_flow", "consumption", "instant_flow"]
+df.columns = ["stt", "time", "datetime", "pressure", "total_flow", "consumption", "instant_flow"]
 
+df["datetime"] = df["datetime"].str.replace('SA', 'AM').str.replace('CH', 'PM')
+df["datetime"] = pd.to_datetime(df["datetime"], format='%d/%m/%Y %I:%M:%S %p')
 
-# df["datetime"] = df["datetime"].str.replace('SA', 'AM').str.replace('CH', 'PM')
-# df["datetime"] = pd.to_datetime(df["datetime"], format='%d/%m/%Y %I:%M:%S %p')
-# # Chuyển datetime thành kiểu datetime chuẩn
-# df["timestamp"] = pd.to_datetime(df["datetime"], dayfirst=True)
+df["timestamp"] = pd.to_datetime(df["datetime"], dayfirst=True)
 
-# # Bỏ cột không cần
-# df = df.drop(columns=["stt", "time", "datetime"])
+df = df.drop(columns=["stt", "time", "datetime"])
 
-# # Kết nối tới MySQL
-# engine = create_engine("mysql+pymysql://root:123456@localhost:3306/water_leak")
+engine = create_engine("mysql+pymysql://root:123456@localhost:3306/water_leak")
 
-# # Ghi vào DB
-# df.to_sql("sensor_data", engine, if_exists="append", index=False)
+df.to_sql("sensor_data", engine, if_exists="append", index=False)
